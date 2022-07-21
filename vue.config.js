15000452aa2322348bb24c954367d66859e92e80
Navid079 <navid.naseri.079@gmail.com>
2022-07-21 18:22:24 +0430

    repo renamed

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-calculator/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vueCalculator/" : "/",
 };
